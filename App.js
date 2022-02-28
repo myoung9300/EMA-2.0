fe72075419fb74ae4cdc41575afbd656465a21e6
Myoung9300 <myoung9300@icloud.com>
2022-02-28 15:47:51 -0700

    Update (2/28)

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import Amplify from 'aws-amplify';
+
+import awsconfig from './src/aws-exports';
+import TabNavigation from './components/infrastructure/tab/Tab';
+
+Amplify.configure(awsconfig);
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaProvider>
+			<NavigationContainer>
+				<TabNavigation />
+				<StatusBar style='auto' />
+			</NavigationContainer>
+		</SafeAreaProvider>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
